b4247fd5dd1d90667d292c1e308371fbf9535e7d
segawa <>
2023-12-27 15:06:08 +0900

    feat: データフェッチして表示

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,21 +1,37 @@
 import Image from "next/image";
 
 async function getData() {
-  // const res = await fetch("https://jsonplaceholder.typicode.com/todos/1");
-  // const json = await res.json();
-  // return json.userId;
   const res = await fetch("http://localhost:4000/posts");
-  const json = await res.json();
-
-  console.log(json);
-  return json[0].id;
+  const posts = await res.json();
+  console.log(posts);
+  return posts;
 }
+
+const Posts = ({ posts }: { posts: any[] }) => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-5xl font-bold">Posts</h1>
+      <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
+        {posts.map((post: any) => (
+          <a
+            href={`/posts/${post.id}`}
+            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
+            key={post.id}
+          >
+            <h3 className="text-2xl font-bold">{post.title} &rarr;</h3>
+            <p className="mt-4 text-xl">{post.author}</p>
+          </a>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default async function Home() {
   const data = await getData();
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {data}
+      <Posts posts={data} />
     </main>
   );
 }
