15493d68bf14ac8ebb6513562c54fed7d43b6777
segawa <>
2023-12-28 17:00:41 +0900

    feat: バリデーション効くように

--- my-app/src/interfaces/post.ts
+++ my-app/src/interfaces/post.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const Post = z.object({
   id: z.number(),
-  title: z.string(),
+  title: z.string().min(1, "Title is required"),
   author: z.string().nullish(),
 });
 
