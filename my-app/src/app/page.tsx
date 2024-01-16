afbab76faec65d7f957d2e0698a019f6f117c487
segawa <>
2024-01-16 20:35:22 +0900

    add: select component

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,13 +1,32 @@
 import { Suspense, use } from "react";
 import { CreatePostForm } from "@/components/CreatePostForm";
 import { Post } from "@/interfaces/post";
-
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 const Posts = async () => {
   const res = await fetch("http://localhost:4000/posts");
   const posts = await res.json();
   await delay(1);
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <Select>
+        <SelectTrigger className="w-[180px]">
+          <SelectValue placeholder="Theme" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="light">Light</SelectItem>
+          <SelectItem value="dark">Dark</SelectItem>
+          <SelectItem value="system">System</SelectItem>
+        </SelectContent>
+      </Select>
+      <br />
+      <br />
+      <br />
       <h1 className="text-5xl font-bold">Posts</h1>
       <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
         {posts.map((post: Post) => (
