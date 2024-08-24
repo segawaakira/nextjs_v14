1f854ec9a64a1d59d1e20d4fb0b9c93142650543
segawa <>
2024-08-24 15:05:55 +0900

    Update page.tsx

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -8,9 +8,17 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-const Posts = async () => {
+
+type Props = {
+  searchParams: {
+    title?: string[];
+  };
+};
+
+const Posts = async ({ searchParams }: Props) => {
   const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}/posts`);
   const posts = await res.json();
+  console.log(searchParams);
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Select>
@@ -43,12 +51,12 @@ const Posts = async () => {
   );
 };
 
-export default async function Home() {
+export default async function Home({ searchParams }: Props) {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <CreatePostForm />
       <Suspense fallback={<p>Loading...</p>}>
-        <Posts />
+        <Posts searchParams={searchParams} />
       </Suspense>
     </main>
   );
